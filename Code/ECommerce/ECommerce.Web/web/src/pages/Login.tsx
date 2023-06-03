de9da6e34eff280ec718b580e21cc55f0814f915
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-03 21:11:36 +0800

    [u] Changes

--- Code/ECommerce/ECommerce.Web/web/src/pages/Login.tsx
+++ Code/ECommerce/ECommerce.Web/web/src/pages/Login.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
 const Login = () => {
-  return (
-    <div className='flex'>
-        
-        
+    return (
 
-    </div>
-  )
+        <div className='w-full h-screen flex justify-center items-center'>
+            <section className='p-5 border rounded w-96'>
+                <header className='w-full text-xl font-medium pb-2'>
+                    Login your Account
+                </header>
+                <div>
+                    <label className='text-sm'>Logon Name:</label>
+                    <input className='w-full px-2 py-1.5 my-2 border rounded' type='text' placeholder='Enter your username or email' />
+                </div>
+                <div>
+                    <label className='text-sm'>Password:</label>
+                    <input className='w-full px-2 py-1.5 my-2 border rounded'type='passowrd' placeholder='Enter your password' />
+                </div>
+                
+                <div className='mt-4'>
+                    <button className='w-full bg-blue-500'>Login</button>
+                    <div className='py-2 text-center'>or</div>
+                    <button className='w-full bg-blue-500'>Register</button>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Login
