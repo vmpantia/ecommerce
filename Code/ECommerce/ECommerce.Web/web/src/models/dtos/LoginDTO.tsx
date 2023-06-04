a3c6d7eeb085a01e831c74eaaffa5647a70e0fb7
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-04 09:42:41 +0800

    [u] Changes

---
+++ Code/ECommerce/ECommerce.Web/web/src/models/dtos/LoginDTO.tsx
@@ -0,0 +1,5 @@
+export interface LoginDTO {
+    name:string;
+    email:string;
+    accessToken:string;
+}
---
+++ Code/ECommerce/ECommerce.Web/web/src/models/requests/LoginUserRequest.tsx
@@ -0,0 +1,4 @@
+export interface LoginUserRequest {
+    logonName:string;
+    password:string;
+}
