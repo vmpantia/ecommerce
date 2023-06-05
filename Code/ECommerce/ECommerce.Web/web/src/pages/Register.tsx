217b9d83c58b0b38a27397d948a5763ee71f5a29
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-05 14:48:30 +0800

    [u] changes

--- Code/ECommerce/ECommerce.Web/web/src/pages/Register.tsx
+++ Code/ECommerce/ECommerce.Web/web/src/pages/Register.tsx
@@ -23,72 +23,73 @@ const Register = () => {
     }
 
     return (
-        <div className='w-full h-screen flex justify-center items-center bg-gray-100'>
-            <section className='p-9 border rounded bg-white drop-shadow-xl w-1/2'>
-                <header className='w-full flex text-2xl font-medium pb-3 mb-2 border-b'>
+        <div className='flex justify-center'>
+            <section className='w-1/2 p-9 mt-16 border rounded bg-white drop-shadow-xl '>
+                <header className='w-full flex text-2xl font-medium border-b pb-3'>
                     <UserPlusIcon className='w-6 mr-2 mt-1'/>
                     Register Account
                 </header>
+
+                <section className="font-medium pt-3">User Details</section>
+                <section className="mt-3 grid md:grid-cols-1 lg:grid-cols-2 gap-3">
+                    <InputField type="text" 
+                                placeholder="Enter your username" 
+                                required={true}
+                                name="username"
+                                label="Username"
+                                value={user.userName}
+                                onValueChangedHandler={onValueChange} />
+                    <InputField type="email" 
+                                placeholder="Enter your email" 
+                                required={true}
+                                name="email"
+                                label="Email"
+                                value={user.email}
+                                onValueChangedHandler={onValueChange} />
+                    <InputField type="password" 
+                                placeholder="Enter your password" 
+                                required={true}
+                                name="password"
+                                label="Password"
+                                value={user.password}
+                                onValueChangedHandler={onValueChange} />
+                    <InputField type="password" 
+                                placeholder="Enter your password" 
+                                required={true}
+                                name="password"
+                                label="Confirm Password"
+                                value={confirmPassword}
+                                onValueChangedHandler={(e) => setConfirmPassword(e.target.value)} />
+                </section>
                 
-                <section className="grid sm:grid-cols-1 md:grid-cols-2 gap-6">
-                    <div>
-                        <InputField type="text" 
-                                    placeholder="Enter your username" 
-                                    required={true}
-                                    name="username"
-                                    label="Username"
-                                    value={user.userName}
-                                    onValueChangedHandler={onValueChange} />
-                        <InputField type="email" 
-                                    placeholder="Enter your email" 
-                                    required={true}
-                                    name="email"
-                                    label="Email"
-                                    value={user.email}
-                                    onValueChangedHandler={onValueChange} />
-                        <InputField type="password" 
-                                    placeholder="Enter your password" 
-                                    required={true}
-                                    name="password"
-                                    label="Password"
-                                    value={user.password}
-                                    onValueChangedHandler={onValueChange} />
-                        <InputField type="password" 
-                                    placeholder="Enter your password" 
-                                    required={true}
-                                    name="password"
-                                    label="Confirm Password"
-                                    value={confirmPassword}
-                                    onValueChangedHandler={(e) => setConfirmPassword(e.target.value)} />
-                    </div>
-                    <div>
-                        <InputField type="text" 
-                                    placeholder="Enter your first name" 
-                                    required={true}
-                                    name="firstName"
-                                    label="First Name"
-                                    value={user.firstName}
-                                    onValueChangedHandler={onValueChange} />
-                        <InputField type="text" 
-                                    placeholder="Enter your middle name" 
-                                    name="middleName"
-                                    label="Middle Name"
-                                    value={user.middleName}
-                                    onValueChangedHandler={onValueChange} />
-                        <InputField type="text" 
-                                    placeholder="Enter your last name" 
-                                    required={true}
-                                    name="lastName"
-                                    label="Last Name"
-                                    value={user.lastName}
-                                    onValueChangedHandler={onValueChange} />
-                        <InputField type="date" 
-                                    required={true}
-                                    name="birthDate"
-                                    label="Birthdate"
-                                    value={user.birthDate}
-                                    onValueChangedHandler={onValueChange} />
-                    </div>
+                <section className="font-medium pt-3">Personal Details</section>
+                <section className="mt-3 grid md:grid-cols-1 lg:grid-cols-2 gap-3">
+                    <InputField type="text" 
+                                placeholder="Enter your first name" 
+                                required={true}
+                                name="firstName"
+                                label="First Name"
+                                value={user.firstName}
+                                onValueChangedHandler={onValueChange} />
+                    <InputField type="text" 
+                                placeholder="Enter your middle name" 
+                                name="middleName"
+                                label="Middle Name"
+                                value={user.middleName}
+                                onValueChangedHandler={onValueChange} />
+                    <InputField type="text" 
+                                placeholder="Enter your last name" 
+                                required={true}
+                                name="lastName"
+                                label="Last Name"
+                                value={user.lastName}
+                                onValueChangedHandler={onValueChange} />
+                    <InputField type="date" 
+                                required={true}
+                                name="birthDate"
+                                label="Birthdate"
+                                value={user.birthDate}
+                                onValueChangedHandler={onValueChange} />
                 </section>
                 
                 <div className='mt-4'>
