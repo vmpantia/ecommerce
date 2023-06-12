3726279841ab8c443dc732dc6879ca36550518a1
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-12 13:36:22 +0800

    [u] Changes

--- Code/ECommerce/ECommerce.Web/web/src/pages/Layout.tsx
+++ Code/ECommerce/ECommerce.Web/web/src/pages/Layout.tsx
@@ -1,10 +1,25 @@
 //Services
+import { UserIcon } from '@heroicons/react/24/solid';
 import { getClient } from '../services/UserService'
 
 const Layout = () => {
     let client = getClient();
     return (
-        <div>{client.email}</div>
+        <div>
+            <header className='w-full h-16 px-5  bg-gray-800 flex justify-between place-items-center'>
+                {/* Company Logo */}
+                <section>
+                </section>
+                {/* Client Details */}
+                <section className='inline-flex text-white text-sm'>
+                    <div className='mr-4'>
+                        <div className='w-full'>{client.email}</div>
+                        <div className='w-full text-right font-medium'>{client.name}</div>
+                    </div>
+                    <UserIcon className='h-8 border rounded-full mt-1.5'/>
+                </section>
+            </header>
+        </div>
     )
 }
 
