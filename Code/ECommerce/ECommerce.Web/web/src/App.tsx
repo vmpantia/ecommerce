821c0266dfa43e5985302f98eb104cc1b9878565
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-12 13:08:48 +0800

    [u] Changes

--- Code/ECommerce/ECommerce.Web/web/src/App.tsx
+++ Code/ECommerce/ECommerce.Web/web/src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
+//Components
+import PrivateRoute from "./components/PrivateRoute"
+
 //Pages
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import Layout from "./pages/Layout"
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/login"    element={<Login />}/>
-      <Route path="/register"    element={<Register />}/>
-      <Route path="/"               element={<Login />}>
+      <Route path="/register" element={<Register />}/>
+      <Route path="/"         element={<PrivateRoute><Layout /></PrivateRoute>}>
       </Route>
     </Routes>
   </BrowserRouter>
