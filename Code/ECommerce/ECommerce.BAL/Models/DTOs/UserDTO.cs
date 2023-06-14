6a4393fa213b0c6a2c512732992ac7e181d36508
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-06-14 17:43:05 +0800

    [u] Changes

--- Code/ECommerce/ECommerce.BAL/Models/DTOs/UserDTO.cs
+++ Code/ECommerce/ECommerce.BAL/Models/DTOs/UserDTO.cs
@@ -1,20 +1,21 @@
 ﻿using ECommerce.BAL.Validator;
+using ECommerce.DAL.DataAccess.Entities;
 using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
 namespace ECommerce.BAL.Models.DTOs
 {
-    public class UserDTO
+    public class UserDTO : User
     {
         [Required] public Guid InternalID { get; set; }
         [Required, MaxLength(15)] public string Username { get; set; }
-        [EmailAddress, MaxLength(50)] public string Email { get; set; }
+        [Required, EmailAddress, MaxLength(50)] public string Email { get; set; }
         [Required, MaxLength(100)] public string Password { get; set; }
         [Required, MaxLength(15)] public string Role { get; set; }
-        [Display(Name="First Name"), Required, MaxLength(40)] public string FirstName { get; set; }
+        [Display(Name = "First Name"), Required, MaxLength(40)] public string FirstName { get; set; }
         [Display(Name = "Middle Name"), MaxLength(40)] public string? MiddleName { get; set; }
         [Display(Name = "Last Name"), Required, MaxLength(40)] public string LastName { get; set; }
-        [BirthDateValidator] public string BirthDate { get; set; }
+        [Display(Name = "Birth Date"), Required, BirthDateValidator] public string BirthDate { get; set; }
 
         //Profile
         public string? Profile { get; set; }
--- Code/ECommerce/ECommerce.BAL/Validator/BirthDateValidator.cs
+++ Code/ECommerce/ECommerce.BAL/Validator/BirthDateValidator.cs
@@ -5,19 +5,17 @@ namespace ECommerce.BAL.Validator
 {
     public class BirthDateValidator : ValidationAttribute
     {
-
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            DateTime date;
-            if (value == null || string.IsNullOrEmpty(value.ToString()))
-                return new ValidationResult(Error.ATTR_USR_BIRTHDATE_REQUIRED);
+            if (value != null)
+            {
+                DateTime date;
+                if (!DateTime.TryParse(value.ToString(), out date))
+                    return new ValidationResult(Error.ATTR_USR_BIRTHDATE_VALID_DATE);
 
-            if (!DateTime.TryParse(value.ToString(), out date))
-                return new ValidationResult(Error.ATTR_USR_BIRTHDATE_VALID_DATE);
-
-            if (date >= DateTime.Today)
-                return new ValidationResult(Error.ATTR_USR_BIRTHDATE_PAST_DATE);
-
+                if (date >= DateTime.Today)
+                    return new ValidationResult(Error.ATTR_USR_BIRTHDATE_PAST_DATE);
+            }
             return ValidationResult.Success;
         }
     }
